9deb17ab732027d6bee454ecf22e92c559b4692e
Sabiq Al Hasby <sabiqxs@gmail.com>
2017-03-23 04:29:43 +0700

    p

---
+++ src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import {RouterModule, Routes} from "@angular/router";
+import {CollectionComponent} from "./collection/collection.component";
+import {MarketComponent} from "./market/market.component";
+const APP_ROUTES: Routes = [
+  {path: '', redirectTo: '/collection', pathMatch: 'full'},
+  {path: 'collection', component: CollectionComponent},
+  {path: 'market', component: MarketComponent},
+];
+
+export const routing= RouterModule.forRoot(APP_ROUTES);
---
+++ src/app/shared/collectable.service.ts
@@ -0,0 +1,30 @@
+import {Collectable} from "./collectable.model";
+
+export class CollectableService{
+  private collectables: Collectable[] =[
+    { description: 'A very rare copy of "jquery for dummies"', type:'Book'},
+    { description: 'The first Letter ever written', type:'Piece of paper'},
+    { description: 'A photograph showing nothing', type:'Photo'},
+    { description: 'A box with all sold Zunes', type:'Garbage'}
+
+  ];
+
+  private  collection: Collectable[] = [];
+
+  getCollectables(){
+    return this.collectables;
+  }
+  getCollection(){
+    return this.collection;
+  }
+
+  addToCollection(item: Collectable){
+    if(this.collection.indexOf(item) !== -1){
+      return;
+    }
+    this.collection.push(item)
+  }
+  removeFromCollection(item: Collectable){
+    this.collection.splice(this.collection.indexOf(item), 1);
+  }
+}
